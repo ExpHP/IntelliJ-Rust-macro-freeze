b50a02a27935c9fb4e862c642e78aa65d36f8001
Michael Lamparski <diagonaldevice@gmail.com>
2018-05-29 15:10:26 -0400

    minimize

--- src/index_vec.rs
+++ src/index_vec.rs
@@ -2,13 +2,6 @@
 use std::fmt::{self, Debug};
 use std::hash::Hash;
 
-pub unsafe trait Idx {
-}
-
-unsafe impl Idx for usize {
-
-}
-
 #[macro_export]
 macro_rules! newtype_index {
     ($type:ident) => (
