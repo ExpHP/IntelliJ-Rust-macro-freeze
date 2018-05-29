498f7d4cb261bc8dc91a6e491e2cc73fef1c4162
Michael Lamparski <diagonaldevice@gmail.com>
2018-05-29 15:04:00 -0400

    minimize

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,3 @@ newtype_index!{
     #[derive(Serialize, Deserialize)]
     Layer
 }
-
-// (For some reason, both this and the impl seem to be needed for the lockup)
-pub trait MetaSift<Targets, Indices> {
-    fn sift(&self) -> Targets;
-}
-
-impl<List, Targets, Indices> MetaSift<Targets, Indices> for List
-    where
-        List: Clone,
-        List: ::frunk::hlist::Sculptor<Targets, Indices>,
-{
-    fn sift(&self) -> Targets
-    { self.clone().sculpt().0 }
-}
