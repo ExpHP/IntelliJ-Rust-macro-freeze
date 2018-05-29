7f705fee8dd2e647bd638d5446d942ff69481d96
Michael Lamparski <diagonaldevice@gmail.com>
2018-05-29 15:09:36 -0400

    minimize

--- src/index_vec.rs
+++ src/index_vec.rs
@@ -12,7 +12,6 @@ unsafe impl Idx for usize {
 #[macro_export]
 macro_rules! newtype_index {
     ($type:ident) => (
-        #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
         pub struct $type(usize);
 
     );
